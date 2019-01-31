b88c9c2e201e8ccb052ccd76a0a559bf0d2c869b
Collin Argo <collinargo@gmail.com>
2019-01-31 11:28:24 -0500

    fix header spacing

--- src/components/header.js
+++ src/components/header.js
@@ -1,78 +1,22 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import { Link } from 'gatsby'
 
-// import NavLink from './navLink'
+import NavLink from './navLink'
 
 const Header = ({ siteTitle }) => (
   <div
-  className='Header'
+    className='Header'
     style={{
       // background: `rebeccapurple`,
       // marginBottom: `1.45rem`,
       padding: `1.45rem 1.0875rem`,
     }}
   >
-    <h1 style={{ margin: 0 }}>
-      <Link
-        to="/"
-        className='Link' activeClassName=' isCurrent'
-        // style={{
-        //   color: `white`,
-        //   textDecoration: `none`,
-        // }}
-      >
-        {'Home'}
-      </Link>
-    </h1>
-    <h1 style={{ margin: 0 }}>
-      <Link
-        to="/TaskMagic"
-        className='Link' activeClassName=' isCurrent'
-        // style={{
-        //   color: `white`,
-        //   textDecoration: `none`,
-        // }}
-      >
-        {'Task Magic'}
-      </Link>
-    </h1>
-    <h1 style={{ margin: 0 }}>
-      <Link 
-        to="/TeeTimes/"
-        className='Link' activeClassName=' isCurrent'
-        // style={{
-        //   color: `white`,
-        //   textDecoration: `none`,
-        // }}
-        >
-          {'Tee Times'}
-        </Link>
-    </h1>
-    <h1 style={{ margin: 0 }}>
-      <Link 
-        to="/FFScheduler"
-        className='Link' activeClassName=' isCurrent'
-        // style={{
-        //   color: `white`,
-        //   textDecoration: `none`,
-        // }}
-        >
-          {'FFScheduler'}
-        </Link>
-    </h1>
-    <h1 style={{ margin: 0 }}>
-      <Link 
-        to="/SpaceOut"
-        className='Link' activeClassName=' isCurrent'
-        // style={{
-        //   color: `white`,
-        //   textDecoration: `none`,
-        // }}
-        >
-          {'Space Out'}
-        </Link>
-    </h1>
+    <NavLink to='/' text='Home'/>
+    <NavLink to='/TaskMagic' text='Task Magic'/>
+    <NavLink to='/TeeTimes' text='TeeTimes'/>
+    <NavLink to='/FFScheduler' text='FFScheduler'/>
+    <NavLink to='/SpaceOut' text='SpaceOut'/>
   </div>
 )
 
--- src/components/navLink.js
+++ src/components/navLink.js
@@ -3,18 +3,17 @@ import { Link } from 'gatsby'
 
 export default function NavLink(props) { 
     return (
-        <Link className='NavLink' activeClassName=' isCurrent'
-        {...props}
-        // getProps={({ isCurrent }) => {
-        //     // the object returned here is passed to the
-        //     // anchor element's props
-        //     return {
-        //         className: `NavLink${isCurrent ? ` isCurrent` : ``}`,
-        //         // style: {
-        //         //     color: isCurrent ? "lightsalmon" : "deepskyblue"
-        //         // }
-        //     };
+    <h1 style={{ margin: 0 }}>
+      <Link
+        to={props.to}
+        className='Link' activeClassName=' isCurrent'
+        // style={{
+        //   color: `white`,
+        //   textDecoration: `none`,
         // }}
-        />
+      >
+        {props.text}
+      </Link>
+    </h1>
     )
 }
